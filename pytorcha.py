144df31db2462dd58efeb9c2588b1019e6466598
JahongLiu <jahongliu@gmail.com>
2023-04-24 20:35:22 -0400

    import dependencies

---
+++ pytorcha.py
@@ -0,0 +1,8 @@
+# dependencies
+from torch import nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import Totensor
+
+
