84fde04e93d00c8760d7f41701ccd40248573203
JahongLiu <jahongliu@gmail.com>
2023-04-24 20:50:38 -0400

    finish and clean up

--- pytorcha.py
+++ pytorcha.py
@@ -1,9 +1,11 @@
-# dependencies
-from torch import nn
+# import dependencies
+import torch 
+from PIL import Image
+from torch import nn, save, load
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import Totensor
+from torchvision.transforms import ToTensor
 
 # Get data 
 train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
@@ -24,6 +26,7 @@ class ImageClassifier(nn.Module):
             nn.Flatten(), 
             nn.Linear(64*(28-6)*(28-6), 10)  
         )
+        
     def forward(self, x): 
         return self.model(x)
     
@@ -32,3 +35,29 @@ clf = ImageClassifier().to('cuda')
 opt = Adam(clf.parameters(), lr=1e-3)
 loss_fn = nn.CrossEntropyLoss() 
     
+# Training flow 
+if __name__ == "__main__": 
+    for epoch in range(10): # train for 10 epochs
+        for batch in dataset: 
+            X,y = batch 
+            X, y = X.to('cuda'), y.to('cuda') 
+            yhat = clf(X) 
+            loss = loss_fn(yhat, y) 
+
+            # Apply backprop 
+            opt.zero_grad()
+            loss.backward() 
+            opt.step() 
+
+        print(f"Epoch:{epoch} loss is {loss.item()}")
+    
+    with open('model_state.pt', 'wb') as f: 
+        save(clf.state_dict(), f) 
+
+    with open('model_state.pt', 'rb') as f: 
+        clf.load_state_dict(load(f))  
+
+    img = Image.open('President_Xi.jpeg') 
+    img_tensor = ToTensor()(img).unsqueeze(0).to('cuda')
+
+    print(torch.argmax(clf(img_tensor)))
